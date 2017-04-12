fbee8029566696cc7fa3df179d9efb113fdcff39
k.tada <white.rv0925@gmail.com>
2017-04-12 16:03:41 +0900

    テスト

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,6 +1,6 @@
-bind \cs '__pet-select --layout=bottom-up'
+bind \cs '__pet-select'
 
 if bind -M insert >/dev/null ^/dev/null
-  bind -M insert \cs '__pet-select --layout=bottom-up'
+  bind -M insert \cs '__pet-select'
 end
 
